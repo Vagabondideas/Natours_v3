04afa5eae1eed440b5ca37c6434da5ab07a02d15
Vagabondideas <whitesectora@gmail.com>
2024-07-03 15:42:02 -0400

    Clean Code

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,24 +1,9 @@
-/* eslint-disable */
-// import axios from 'axios';
 import { showAlert } from './alerts';
 
 export const bookTour = async (tourId) => {
   try {
     // 1) Get checkout session from API
     const response = await fetch(`/api/v1/bookings/checkout-session/${tourId}`);
-    /*
-    THIS METHOD (Std by Stripe and ChatGPT) WILL CAUSE ERROR, Not able to retrieve session
-    const response = await fetch(
-      `/api/v1/bookings/checkout-session/${tourId}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-    );
-    */
-
     const session = await response.json();
 
     // 2) Create checkout form + chanrge credit card
