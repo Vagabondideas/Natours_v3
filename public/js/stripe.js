29e0f7a5566a0f99afae5d88891e0d8e87c34d3e
Vagabondideas <whitesectora@gmail.com>
2024-07-04 23:41:31 -0400

    load stripe

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,11 +1,11 @@
 import { showAlert } from './alerts';
 
 // import axios from 'axios';
-// import { loadStripe } from '@stripe/stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
-// const stripe = await loadStripe(
-//   'pk_test_51PROQXIQVsIi8CVD8lxUYKyx9z4Vsa1B25T059hz05JZKlG3txwAivE8ZUxPwM4tTzBczuQttjJkqg9jFIPTsb8c00HdSWRWfH',
-// );
+const stripe = await loadStripe(
+  'pk_test_51PROQXIQVsIi8CVD8lxUYKyx9z4Vsa1B25T059hz05JZKlG3txwAivE8ZUxPwM4tTzBczuQttjJkqg9jFIPTsb8c00HdSWRWfH',
+);
 
 // stripe.redirectToCheckout({ sessionId });
 
@@ -18,12 +18,13 @@ export const bookTour = async (tourId) => {
     console.log('Session URL at frontend from bookTour ' + session.url);
 
     // 2) Create checkout form + chanrge credit card
-    if (session.url) {
-      window.location.href = session.url;
-      console.log('Session URL at frontend from bookTour ' + session.url);
-    } else {
-      throw new Error('Session URL not found');
-    }
+    stripe.redirectToCheckout({ session });
+    // if (session.url) {
+    //   window.location.href = session.url;
+    //   console.log('Session URL at frontend from bookTour ' + session.url);
+    // } else {
+    //   throw new Error('Session URL not found');
+    // }
   } catch (err) {
     console.log(err);
     showAlert('error', err);
